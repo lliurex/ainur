56e587ef7e876ecccc7b7a504d86ef1ade35eda1
Raul Rodrigo Segura <raurodse@gmail.com>
2020-06-10 08:34:47 +0200

    add requirements

--- ainur/modules/pages/__init__.py
+++ ainur/modules/pages/__init__.py
@@ -1,10 +1,7 @@
 from flask import Blueprint, render_template, abort
-from ainur import app
 from jinja2 import TemplateNotFound
 
 simple_page = Blueprint('simple_page', __name__,template_folder='templates')
-
-
 
 @simple_page.route('/', defaults={'page': 'index'})
 @simple_page.route('/<page>')
