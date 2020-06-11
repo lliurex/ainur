4e348eaf1a24a4601bd1139742ed9631a57620d1
Raul Rodrigo Segura <raurodse@gmail.com>
2020-06-11 21:57:04 +0200

    access correctly to config value

--- src/ainur/__init__.py
+++ src/ainur/__init__.py
@@ -18,16 +18,16 @@ babel = Babel(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-modules_path = os_path_join(app.config.BASE_PATH,'ainur','modules')
+modules_path = os_path_join(app.config['BASE_PATH'],'ainur','modules')
 for module_name in listdir(modules_path):
-    module_spec = imp_util.spec_from_file_location('ainur.modules.'+module_name, os_path_join(modules_path,module_name,'__init__.py'))
+    module_spec = imp_util.spec_from_file_location('ainur.modules.'+module_name, os_path_join(modules_path,module_name,'__init__.py')
     temp_module = imp_util.module_from_spec(module_spec)
     module_spec.loader.exec_module(temp_module)
     app.register_blueprint(getattr(temp_module,'exportmodule'),url_prefix='/{module_name}'.format(module_name=module_name))
 
-adminmodules_path = os_path_join(app.config.BASE_PATH,'ainur','adminmodules')
+adminmodules_path = os_path_join(app.config['BASE_PATH'],'ainur','adminmodules')
 for module_name in listdir(adminmodules_path):
-    module_spec = imp_util.spec_from_file_location('ainur.adminmodules.'+module_name, os_path_join(adminmodules_path,module_name,'__init__.py'))
+    module_spec = imp_util.spec_from_file_location('ainur.adminmodules.'+module_name, os_path_join(adminmodules_path,module_name,'__init__.py')
     temp_module = imp_util.module_from_spec(module_spec)
     module_spec.loader.exec_module(temp_module)
     app.register_blueprint(getattr(temp_module,'exportmodule'),url_prefix='/admin/{module_name}'.format(module_name=module_name))
